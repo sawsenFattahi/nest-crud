bbc6041d4df6c53fc970aa258609e604142564f4
sawsen fattahi <sawsen.fattahi@gmail.com>
2021-10-22 08:51:48 +0200

    add class-validator

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 20)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
   password: string;
 }

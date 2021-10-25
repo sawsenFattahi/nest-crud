60ee8d0592080ca5dcc71c39510517097d6a13c1
sawsen fattahi <sawsen.fattahi@gmail.com>
2021-10-25 10:56:43 +0200

    replace promise

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import {
   Req,
   UseGuards,
 } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { RegisterDto } from './dtos/register.dto';
@@ -16,14 +17,16 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() body: RegisterDto) {
+  register(@Body() body: RegisterDto): Observable<string> {
     return this.authService.register(body);
   }
 
   @HttpCode(200)
   @UseGuards(LocalAuthenticationGuard)
   @Post('login')
-  login(@Req() request: { user: LoginDto }) {
+  login(
+    @Req() request: { user: LoginDto },
+  ): Observable<{ access_token: string }> {
     return this.authService.login(request.user);
   }
 }
